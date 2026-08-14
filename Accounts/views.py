bfa09f81a86453c8102f8ac5e02b500bf7543e9f
Mufeed2255 <npmufeed7@gmail.com>
2026-08-14 15:48:20 +0530

    added

--- Accounts/views.py
+++ Accounts/views.py
@@ -1621,3 +1621,7 @@ def set_default_address(request, id):
     )
 
     return redirect("address_list")
+
+def sample_view(request):
+    num=1+2
+    return render(request,'accounts/sample.html',{'num':num})
